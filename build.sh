7ada64cc75463a48f0fa860d42fd59003edbd3ca
gregsadetsky <lepetitg@gmail.com>
2024-03-23 20:38:51 -0400

    .

--- build.sh
+++ build.sh
@@ -1,12 +1,1 @@
-# `chown` below necessary because of issue reported here
-# https://github.com/envygeeks/jekyll-docker/issues/268
-# alternatively, see
-# https://michaelcurrin.github.io/code-cookbook/recipes/ci-cd/github-actions/workflows/jekyll/build/container.html
-
-pwd
-ls -ltra
-
-
-chown jekyll:jekyll -R /repo
-
-jekyll build -d $DISCO_DIST_PATH --trace
+jekyll build -d $DISCO_DIST_PATH
