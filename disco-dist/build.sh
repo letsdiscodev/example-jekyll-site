29c72ad0654b3b387181d1bfc6935e0cc692cc60
gregsadetsky <lepetitg@gmail.com>
2024-03-23 20:20:17 -0400

    .

--- disco-dist/build.sh
+++
@@ -1,7 +0,0 @@
-# `chown` below necessary because of issue reported here
-# https://github.com/envygeeks/jekyll-docker/issues/268
-# alternatively, see
-# https://michaelcurrin.github.io/code-cookbook/recipes/ci-cd/github-actions/workflows/jekyll/build/container.html
-
-chown jekyll:jekyll -R /repo
-jekyll build -d $DISCO_DIST_PATH --trace
